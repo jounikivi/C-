a5b7f25de24720452eb6d2d64fd635b1a7c3aa4d
Jouni Kiviperä <jounikivipera@gmail.com>
2024-04-16 07:46:29 +0300

    todolist sovelluslisätty

---
+++ todolist/todolist/todolist/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace todolist
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<TodoItem> todoItems;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            todoItems = new ObservableCollection<TodoItem>();
+            todoList.ItemsSource = todoItems;
+        }
+
+        void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            todoItems.Add(new TodoItem { Name = entry.Text });
+            entry.Text = string.Empty;
+        }
+    }
+}
